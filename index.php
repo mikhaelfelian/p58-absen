d41d51fcf34c9c47e36ddb46415e25f5988542aa
Mikhael Felian Waskito <mikhaelfelian@gmail.com>
2025-10-18 22:02:08 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <?php
-aa
+
 /*
  *---------------------------------------------------------------
  * SHOW ALL PHP ERRORS
  *---------------------------------------------------------------
  */
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
 
 /*
